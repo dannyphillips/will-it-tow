9eb56bb43495bdef982e9430026475a44463caeb
Danny Phillips <dannyp@procore.com>
2018-10-31 14:26:16 -0700

    v1 of tow calculator

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
-import Button from '../components/Button';
+import Button from '../components/Button/Button';
+import style from './style.css'
+import Card from '../components/Card/Card';
 class App extends Component {
   state = {  }
   render() {
     return (
-      <div>
-      <Button/>
+      <div className={style.app}>
+        <div className={style.title}>
+          Will it Tow?
+        </div>
+        <div className={style.cardContainer}>
+          <Card title={"Tow Vehicle"}>
+            <label>Tow Capacity</label>
+            <input value={"asfd"}/>
+          </Card>
+          <Card title={"Risk Rating"}>
+            <label>Tow Risk</label>
+            <input value={"asfd"}/>
+          </Card>
+          <Card title={"Trailer"}>
+            <label>Dry / Unloaded Vehicle Weight(UVW)</label>
+            <input value={"asfd"}/>
+          </Card>
+        </div>
       </div>
     );
   }
