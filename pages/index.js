0eeba8241ea9f46208f04264917b22f19458708c
Danny Phillips <danny.phillips8@gmail.com>
2019-07-01 22:59:10 -0700

    add images and setup state for calculator

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,52 @@
 import React, { Component } from 'react';
-import Button from '../components/Button/Button';
 import style from './style.css'
-import Card from '../components/Card/Card';
+import { Button, Card } from '../components';
 class App extends Component {
-  state = {  }
+  state = {
+    tow_cap: "",
+    trailer_weight: "",
+  }
+  calcRisk() {
+    return this.state.tow_cap > this.state.trailer_weight ? "low" : "high"
+  }
+
   render() {
     return (
       <div className={style.app}>
         <div className={style.title}>
+          <Button />
           Will it Tow?
         </div>
         <div className={style.cardContainer}>
           <Card title={"Tow Vehicle"}>
             <label>Tow Capacity</label>
-            <input value={"asfd"}/>
+            <input
+              type="number"
+              value={this.state.tow_cap}
+              onChange={evt =>
+                this.setState({
+                  tow_cap: evt.target.value,
+                  trailer_weight: evt.target.value/4
+                })
+              }
+            />
           </Card>
           <Card title={"Risk Rating"}>
             <label>Tow Risk</label>
-            <input value={"asfd"}/>
+            {this.calcRisk()}
           </Card>
           <Card title={"Trailer"}>
             <label>Dry / Unloaded Vehicle Weight(UVW)</label>
-            <input value={"asfd"}/>
+            <input
+              type="number"
+              value={this.state.trailer_weight}
+              onChange={evt =>
+                this.setState({
+                  trailer_weight: evt.target.value,
+                  tow_cap: evt.target.value/3
+                })
+              }
+            />
           </Card>
         </div>
       </div>
